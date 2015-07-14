215810b75bf65b4d2781361b46d75997db0b7c75
Mark Brough <mark.brough@gmail.com>
2015-07-14 17:22:34 +0200

    Add further processing to roll up o/w rows

--- eiti/scripts/parse_eiti.py
+++ eiti/scripts/parse_eiti.py
@@ -67,14 +67,53 @@ def parse_eiti(filename):
         csv_total.writeheader()
         
         seen_report_ID = ""
-        for row in csvf:
+        
+        known_data = []
+        
+        for i, row in enumerate(csvf):
             if row["Report ID"] != seen_report_ID:
                 # Process total row
                 make_row(csv_total, row, H_TOTAL)
                 seen_report_ID = row["Report ID"]
             else:
                 if row["Total line?"] == "Total": continue
-                make_row(csv_data, row, H_DATA)
+                known_data.append((i, row))
+                
+    kd = dict(known_data)
+    
+    # Roll up o/w rows
+    ow = False
+    ow_prod_vol = []
+    ow_prod_vol_metric = []
+    ow_source = []
+    prod_vol_col = "Total Production Volume (external source)"
+    prod_vol_metric_col = "Volume Metric"
+    source_col = 'Source for Total Production Volume & Price Information'
+    for i, d in sorted(kd.items(), reverse=True):
+        if d['Commodity'].startswith(("  o/w", "o/w")):
+            # If row starts with o/w then roll up prod vol and get source
+            ow = True
+            if d[prod_vol_col] != "":
+                ow_prod_vol.append(float(d[prod_vol_col]))
+            if d[source_col] != "":
+                ow_source.append(d[source_col])
+            if d[prod_vol_metric_col] != "":
+                ow_prod_vol_metric.append(d[prod_vol_metric_col])
+            # Delete this row
+            kd.pop(i)
+            continue
+        if ow:
+            d[prod_vol_col] = sum(ow_prod_vol)
+            d[prod_vol_metric_col] = "; ".join(set(ow_prod_vol_metric))
+            d[source_col] = "%s; %s" % (d[source_col], 
+                                          "; ".join(set(ow_source)))
+            ow = False
+            ow_prod_vol = []
+            ow_prod_vol_metric = []
+            ow_source = []
+            
+    for i, d in kd.items():
+        make_row(csv_data, d, H_DATA)
 
 if __name__ == '__main__':
     parse_eiti(FILE_EITI)
